a196fd96bb29ca6b8764ce38ebaca2bc31cacc10
Abtolo <102583032+Abtolo@users.noreply.github.com>
2025-01-24 21:33:35 +0100

    Html & css done
    Finally decided on the HTML and CSSc layouts and ended up making a simple layout

---
+++ Script.js
@@ -0,0 +1,7 @@
+"use strict";
+
+const rock = document.getElementById("photo--1");
+const paper = document.getElementById("photo--2");
+const scissors = document.getElementById("photo--3");
+
+rock.addEventListener("");
