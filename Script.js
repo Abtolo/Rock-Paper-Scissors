a67e56e160070b08755a3397a063b603c44c041f
Abtolo <102583032+Abtolo@users.noreply.github.com>
2025-01-25 16:01:25 +0100

    Js Started
    Started working with javascript

--- Script.js
+++ Script.js
@@ -3,5 +3,9 @@
 const rock = document.getElementById("photo--1");
 const paper = document.getElementById("photo--2");
 const scissors = document.getElementById("photo--3");
+const comp = document.querySelector(".computer");
+const player = document.querySelector(".player");
 
-rock.addEventListener("");
+rock.addEventListener("click", function () {
+  const randomNumber = Math.floor(Math.random() * 3) + 1;
+});
