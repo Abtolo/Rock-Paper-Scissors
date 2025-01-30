f14a6a71c235cbd84a6f181bd383603b80d62c3f
Abtolo <102583032+Abtolo@users.noreply.github.com>
2025-01-30 17:37:16 +0100

    Finished
    implemented the restart button so the game restarts and player can play again

--- Script.js
+++ Script.js
@@ -8,6 +8,9 @@ let scores = [0, 0];
 let userScore = document.querySelector(".user-score");
 let compScore = document.querySelector(".comp-score");
 const textScore = document.querySelector(".text-scoring");
+const player = document.querySelector(".player");
+const computer = document.querySelector(".computer");
+const restart = document.querySelector(".restart-btn");
 
 const generateComputerChoice = function (boolean, number) {
   const choices = ["Rock", "Paper", "Scissors"];
@@ -19,39 +22,64 @@ const generateComputerChoice = function (boolean, number) {
 };
 
 const playgame = function (playerChoices, computerChoice) {
-  userChoiceDisplay.setAttribute("src", `Images/${playerChoices}.png`);
-  computerChoiceDisplay.setAttribute("src", `Images/${computerChoice}.png`);
+  if (scores[0] >= 10) {
+    textScore.textContent = "Player wins the Game👌";
+    player.style.backgroundColor = "green";
+    computer.style.backgroundColor = "grey";
+  } else if (scores[1] >= 10) {
+    textScore.textContent = "Computer wins the Game😭";
+    player.style.backgroundColor = "grey";
+    computer.style.backgroundColor = "green";
+  } else {
+    userChoiceDisplay.setAttribute("src", `Images/${playerChoices}.png`);
+    computerChoiceDisplay.setAttribute("src", `Images/${computerChoice}.png`);
 
-  console.log(scores[0]);
+    console.log(scores[0]);
 
-  if (playerChoices === "Rock" && computerChoice === "Scissors") {
-    textScore.textContent = "You Win, 1 point for you";
-    scores[0] += 1;
-    userScore.textContent = "Score : " + scores[0];
-  } else if (playerChoices === "Rock" && computerChoice === "Paper") {
-    textScore.textContent = "Computer Wins, 1 point for computer";
-    scores[1] += 1;
-    compScore.textContent = "Score : " + scores[1];
-  } else if (playerChoices === "Paper" && computerChoice === "Rock") {
-    textScore.textContent = "You Win, 1 point for you";
-    scores[0] += 1;
-    userScore.textContent = "Score : " + scores[0];
-  } else if (playerChoices === "Paper" && computerChoice === "Scissors") {
-    textScore.textContent = "Computer Wins, 1 point for computer";
-    scores[1] += 1;
-    compScore.textContent = "Score : " + scores[1];
-  } else if (playerChoices === "Scissors" && computerChoice === "Rock") {
-    textScore.textContent = "Computer Wins, 1 point for computer";
-    scores[1] += 1;
-    compScore.textContent = "Score : " + scores[1];
-  } else if (playerChoices === "Scissors" && computerChoice === "Paper") {
-    textScore.textContent = "You Win, 1 point for you";
-    scores[0] += 1;
-    userScore.textContent = "Score : " + scores[0];
-  } else if (playerChoices === computerChoice) {
-    textScore.textContent = "It's a Tie";
+    if (playerChoices === "Rock" && computerChoice === "Scissors") {
+      textScore.textContent = "You Win, 1 point for you";
+      scores[0] += 1;
+      userScore.textContent = "Score : " + scores[0];
+    } else if (playerChoices === "Rock" && computerChoice === "Paper") {
+      textScore.textContent = "Computer Wins, 1 point for computer";
+      scores[1] += 1;
+      compScore.textContent = "Score : " + scores[1];
+    } else if (playerChoices === "Paper" && computerChoice === "Rock") {
+      textScore.textContent = "You Win, 1 point for you";
+      scores[0] += 1;
+      userScore.textContent = "Score : " + scores[0];
+    } else if (playerChoices === "Paper" && computerChoice === "Scissors") {
+      textScore.textContent = "Computer Wins, 1 point for computer";
+      scores[1] += 1;
+      compScore.textContent = "Score : " + scores[1];
+    } else if (playerChoices === "Scissors" && computerChoice === "Rock") {
+      textScore.textContent = "Computer Wins, 1 point for computer";
+      scores[1] += 1;
+      compScore.textContent = "Score : " + scores[1];
+    } else if (playerChoices === "Scissors" && computerChoice === "Paper") {
+      textScore.textContent = "You Win, 1 point for you";
+      scores[0] += 1;
+      userScore.textContent = "Score : " + scores[0];
+    } else if (playerChoices === computerChoice) {
+      textScore.textContent = "It's a Tie";
+    }
   }
 };
+
+restart.addEventListener("click", function () {
+  console.log("its working ");
+  userChoiceDisplay.setAttribute("src", "images/blanc.jpeg");
+  computerChoiceDisplay.setAttribute("src", "images/blanc.jpeg");
+  scores[0] = 0;
+  scores[1] = 0;
+
+  userScore.textContent = "Score : ";
+  compScore.textContent = "Score : ";
+
+  textScore.textContent = "Start Playing";
+  player.style.backgroundColor = "blue";
+  computer.style.backgroundColor = "red";
+});
 
 // Heres I determine what the players choice is and the computers choice then I play the game using the function
 for (let index = 0; index < 4; index++) {
