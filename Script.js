66865efbcdcdf867378a1e0a8646543ac3330ee9
Abtolo <102583032+Abtolo@users.noreply.github.com>
2025-01-30 16:52:28 +0100

    Finished gameLogic
    Trial and error but finished the Gamelogic and made it so that the photo changes and theres a text that tells the player if he won or the computer

--- Script.js
+++ Script.js
@@ -1,11 +1,63 @@
 "use strict";
 
-const rock = document.getElementById("photo--1");
-const paper = document.getElementById("photo--2");
-const scissors = document.getElementById("photo--3");
-const comp = document.querySelector(".computer");
-const player = document.querySelector(".player");
+const buttonClicked = document.querySelector(".photoClicked");
+const userChoiceDisplay = document.querySelector(".you-choice");
+const computerChoiceDisplay = document.querySelector(".computer-choice");
+const buttons = document.querySelectorAll(".photoBtn");
+let scores = [0, 0];
+let userScore = document.querySelector(".user-score");
+let compScore = document.querySelector(".comp-score");
+const textScore = document.querySelector(".text-scoring");
 
-rock.addEventListener("click", function () {
-  const randomNumber = Math.floor(Math.random() * 3) + 1;
-});
+const generateComputerChoice = function (boolean, number) {
+  const choices = ["Rock", "Paper", "Scissors"];
+  if (boolean === 0) {
+    return choices[number];
+  }
+  let randomNumber = Math.floor(Math.random() * choices.length);
+  return choices[randomNumber];
+};
+
+const playgame = function (playerChoices, computerChoice) {
+  userChoiceDisplay.setAttribute("src", `Images/${playerChoices}.png`);
+  computerChoiceDisplay.setAttribute("src", `Images/${computerChoice}.png`);
+
+  console.log(scores[0]);
+
+  if (playerChoices === "Rock" && computerChoice === "Scissors") {
+    textScore.textContent = "You Win, 1 point for you";
+    scores[0] += 1;
+    userScore.textContent = "Score : " + scores[0];
+  } else if (playerChoices === "Rock" && computerChoice === "Paper") {
+    textScore.textContent = "Computer Wins, 1 point for computer";
+    scores[1] += 1;
+    compScore.textContent = "Score : " + scores[1];
+  } else if (playerChoices === "Paper" && computerChoice === "Rock") {
+    textScore.textContent = "You Win, 1 point for you";
+    scores[0] += 1;
+    userScore.textContent = "Score : " + scores[0];
+  } else if (playerChoices === "Paper" && computerChoice === "Scissors") {
+    textScore.textContent = "Computer Wins, 1 point for computer";
+    scores[1] += 1;
+    compScore.textContent = "Score : " + scores[1];
+  } else if (playerChoices === "Scissors" && computerChoice === "Rock") {
+    textScore.textContent = "Computer Wins, 1 point for computer";
+    scores[1] += 1;
+    compScore.textContent = "Score : " + scores[1];
+  } else if (playerChoices === "Scissors" && computerChoice === "Paper") {
+    textScore.textContent = "You Win, 1 point for you";
+    scores[0] += 1;
+    userScore.textContent = "Score : " + scores[0];
+  } else if (playerChoices === computerChoice) {
+    textScore.textContent = "It's a Tie";
+  }
+};
+
+// Heres I determine what the players choice is and the computers choice then I play the game using the function
+for (let index = 0; index < 4; index++) {
+  buttons[index].addEventListener("click", function () {
+    const player = generateComputerChoice(0, index);
+    const comp = generateComputerChoice(1, 0);
+    playgame(player, comp);
+  });
+}
